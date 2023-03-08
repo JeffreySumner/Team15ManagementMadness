8a326cc1f3b2fb39863e2af73b4c88ef6107747c
Jeffrey Sumner <jsumner32@gatech.edu>
2023-03-08 16:51:37 -0600

    initial commit for data_gathering functions

--- Code/data_gathering.R
+++ Code/data_gathering.R
@@ -12,11 +12,10 @@ get_all_game_ids <- function(id){
   vec <- c()
   for(i in c("2015-16", "2016-17", "2017-18", "2018-19")){
     vec <- c(get_game_ids(team = id, season = i), vec)
-    }
+  }
   return(vec)
 }
 
 get_all_game_ids("Air Force")
-
 
 
