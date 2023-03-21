3f2eeb21de073f14757100ff222512a0c94d18eb
Jeffrey Sumner <jeffrey.sumner995@gmail.com>
2023-03-20 20:47:42 -0500

    ap poll data

---
+++ Code/data_cleaning_ap_polls.R
@@ -0,0 +1,21 @@
+if (!require('rvest')) install.packages('rvest')
+if (!require('tidyverse')) install.packages('tidyverse')
+
+get_ap_polls <- function(year){
+  print(year)
+  url <- glue::glue("https://www.sports-reference.com/cbb/seasons/men/{year}-polls.html#ap-polls")
+  
+  webpage <- read_html(url)
+  
+  data <- html_table(html_nodes(webpage,"table")[1])[[1]]
+  names(data) <- c("school","conference",1:ncol(data))
+  data <- data %>%
+    mutate(year = year)
+  
+  return(data)
+  
+}
+
+ap_poll_2012_2022_tbl <- lapply(2012:2022, get_ap_polls) %>% bind_rows()
+
+readr::write_csv(ap_poll_2012_2022_tbl,"Data/ap_poll_2012_2022_raw_tbl.csv")
