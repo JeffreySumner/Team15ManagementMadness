4d11610306e1c70d8c839e6eb3ce79362f6f9136
Jeffrey Sumner <jsumner32@gatech.edu>
2023-04-01 09:25:11 -0500

    team stats additions

--- Code/data_cleanup.R
+++ Code/data_cleanup.R
@@ -81,6 +81,111 @@ box_score_clean <- mbb_box_score_2012_2022_tbl %>%
   )
 
 
+# Clean Team Stats ----
+
+box_score_clean1 <- mbb_box_score_2012_2022_tbl %>%
+  separate(
+    field_goals_made_field_goals_attempted
+    , sep = "-"
+    , into = c("FGM", "FGA")
+  ) %>%
+  separate(
+    three_point_field_goals_made_three_point_field_goals_attempted
+    , sep = "-"
+    , into = c("FGM3", "FGA3")
+  ) %>%
+  separate(
+    free_throws_made_free_throws_attempted
+    , sep = "-"
+    , into = c("FTM", "FTA")
+  ) %>%
+  select(
+    game_id
+    , game_date
+    , season
+    , season_type
+    , team_id
+    , opponent_id
+    , home_away
+    , FGM
+    , FGA
+    , FGM3
+    , FGA3
+    , FTM
+    , FTA
+    , offensive_rebounds
+    , defensive_rebounds
+    , total_rebounds
+    , team_rebounds
+    , assists
+    , steals
+    , blocks
+    , turnovers
+    , team_turnovers
+    , total_turnovers
+    , fouls
+    , technical_fouls
+    , flagrant_fouls
+    , largest_lead
+  ) %>%
+  mutate(
+    across(FGM:FTA, as.numeric)
+    , home_away = ifelse(home_away == "HOME", "home", "away")
+  ) %>%
+  pivot_longer(c(-home_away,-game_id,-game_date,-season, -season_type,-team_id,-opponent_id)) 
+
+box_score_clean2 <- box_score_clean1 %>%
+  mutate(home_away = ifelse(home_away == "home","away","home")) %>%
+  rename(opp = value)
+
+team_stats_tbl <- box_score_clean1 %>%
+  left_join(
+    box_score_clean2 %>% select(-team_id, -home_away, -season_type) 
+    , by = c("game_id","game_date","season","name","team_id" = "opponent_id")
+  ) %>%
+  pivot_longer(c(value,opp), names_to = "type") %>%
+  mutate(type = ifelse(type == "value","","_opp")) %>%
+  unite(
+    "temp"
+    , c(name,type)
+    , sep = ""
+  ) %>%
+  pivot_wider(
+    names_from = temp
+    , values_from = value
+  ) %>%
+  # estimate the number of possessions
+  mutate(
+    poss = (FGA - offensive_rebounds) + total_turnovers + (.44 * FTA) # kenpom suggested factor .44
+    , poss_opp = (FGA_opp - offensive_rebounds_opp) + total_turnovers_opp + (.44 * FTA_opp)
+    , points = (FGM - FGM3) * 2 + (FGM3) * 3 + FTM * 1
+    , points_opp = (FGM_opp - FGM3_opp) * 2 + (FGM3_opp) * 3 + FTM_opp * 1
+    , defensive_rating = 100 * (points_opp/poss_opp)
+    , offensive_rating = 100 * (points/poss)
+    , defensive_rating_opp = offensive_rating
+    , offensive_rating_opp = defensive_rating
+  ) %>%
+  pivot_longer(c(-game_id, -game_date, -season, -season_type, -team_id, -opponent_id, -home_away)) %>%
+  group_by(season,team_id,name, game_date) %>%
+  arrange(game_id, .by_group = TRUE) %>%
+  ungroup() %>%
+  group_by(season, team_id, name) %>%
+  mutate(
+    game_number = row_number()
+    , first_game = ifelse(game_number == 1, TRUE, FALSE)
+    , lag1_val = lag(value, 1)
+  ) %>%
+  group_by(season, team_id, name, first_game) %>%
+  mutate(
+    season_avg = cummean(lag1_val)
+  ) %>%
+  # ungroup() %>%
+  # group_by(season, team_id, name) %>%
+  mutate(
+    roll5_val = zoo::rollmean(lag1_val, k = 5,fill = NA, align = "right")
+  )
+
+
 # Clean attendance data and projections ----
 
 attendance_clean <- mbb_attendance_2012_2022_tbl %>%
