5ca5f2c572c5818685cc478695fcc0c83cf8ce10
Jeffrey Sumner <jsumner32@gatech.edu>
2023-04-05 17:21:39 -0500

    initial train/test split

--- Final Code/combined_data.R
+++ Final Code/combined_data.R
@@ -1,4 +1,5 @@
 if (!require('tidyverse')) install.packages('tidyverse')
+if (!require('tidymodels')) install.packages('tidymodels')
 
 ap_poll_tbl <- readr::read_csv("Data/clean/ap_poll_clean_tbl.csv")
 away_team_stats_tbl <- readr::read_csv("Data/clean/away_team_stats_tbl.csv")
@@ -6,6 +7,7 @@ home_team_stats_tbl <- readr::read_csv("Data/clean/home_team_stats_tbl.csv")
 game_information_tbl <- readr::read_csv("Data/clean/game_information_tbl.csv")
 date_tbl <- readr::read_csv("Data/clean/date_tbl.csv")
 
+# Model Data Initial----
 model_data_tbl <- game_information_tbl %>%
   left_join(
     home_team_stats_tbl %>% select(game_id,game_date,season, starts_with("home_"))
@@ -32,7 +34,15 @@ model_data_tbl <- game_information_tbl %>%
     , by = c("season", "week", "awayTeam_id" = "team_id")
   )
 
+# Model Data Readr ----
 # run this if you want to store the data then read on ONLY this section... doesn't matter, up to you
 # this file is too large for github
-readr::write_csv(model_data_tbl, "Data/clean/model_data_tbl.csv")
-model_data_tbl <- readr::read_csv("Data/clean/model_data_tbl.csv")
+# readr::write_csv(model_data_tbl, "Data/clean/model_data_tbl.csv")
+# model_data_tbl <- readr::read_csv("Data/clean/model_data_tbl.csv")
+# Split Model Data ----
+
+set.seed(15) # for reproducibility
+initial_split_data <- initial_split(model_data_tbl, prop = .7)
+train_tbl <- training(initial_split_data)
+test_tbl <- testing(initial_split_data)
+
