8414d225995eb28dbd8c9f63365066f1a05ae209
Jeffrey Sumner <jsumner32@gatech.edu>
2023-04-10 12:50:11 -0500

    adding id column

--- Code/model_eda_starter_script_jeffrey.R
+++ Code/model_eda_starter_script_jeffrey.R
@@ -4,7 +4,29 @@ if (!require('tidymodels')) install.packages('tidymodels')
 model_data_tbl <- readr::read_csv("Data/clean/model_data_tbl.csv")
 
 model_data_tbl <- model_data_tbl %>%
-  mutate(home_winner_response = as.numeric(home_winner))
+  mutate(home_winner_response = as.numeric(home_winner) %>% factor()
+         , away_ap_rank_fct = factor(away_ap_rank_fct)
+         , home_ap_rank_fct = factor(home_ap_rank_fct)
+         , homeTeam_id = factor(homeTeam_id)
+         , awayTeam_id = factor(awayTeam_id)
+         )  %>%
+  select(
+    contains("home_")
+    , contains("away_")
+    , awayTeam_id
+    , homeTeam_id
+    , -contains("city")
+    , -contains("state")
+    , -contains("arena")
+    , -contains("_lat")
+    , -contains("_lon")
+    , -away_ap_rank
+    , -home_ap_rank
+    # , -ends_with("_")
+    , -home_winner
+    # , -away_ap_rank_fct
+    # , -home_ap_rank_fct
+  ) %>% mutate(id = row_number())
 
 set.seed(15) # for reproducibility
 initial_split_data <- initial_split(model_data_tbl, prop = .6)
@@ -25,3 +47,12 @@ DataExplorer::create_report(
   , y = "home_winner_response"
   , report_title = "EDA Report - MBB Home Winner"
   )
+
+# SmartEDA::ExpReport(
+#   model_data_tbl
+#   , Target = "home_winner_response"
+#   , label = NULL
+#   , op_file = "model_data_tbl_smart_eda.html"
+#   , op_dir = "Other Resources/"
+# )
+
