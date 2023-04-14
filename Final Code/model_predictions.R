e5b5e0790f4d4e4527e735fb6dea360e373dff52
Jeffrey Sumner <jsumner32@gatech.edu>
2023-04-14 17:29:22 -0500

    updates

--- Final Code/model_predictions.R
+++ Final Code/model_predictions.R
@@ -48,6 +48,50 @@ validation_split <- initial_split(test_temp_tbl, prop = .5)
 test_tbl <- training(validation_split)
 validation_tbl <- testing(validation_split)
 
+standard_season_test_tbl <- test_tbl %>%
+  select(home_winner_response
+         , contains("season")
+         , -id
+         , home_distance
+         , away_distance
+         # , home_ap_rank_fct
+         # , away_ap_rank_fct
+  )
+names(standard_season_test_tbl) <- gsub("season_avg", "", names(standard_season_test_tbl))
+  
+standard_season_validation_tbl <- validation_tbl %>%
+  select(home_winner_response
+         , contains("season")
+         , -id
+         , home_distance
+         , away_distance
+         # , home_ap_rank_fct
+         # , away_ap_rank_fct
+  )
+names(standard_season_validation_tbl) <- gsub("season_avg", "", names(standard_season_validation_tbl))
+
+standard_rolling_test_tbl <- test_tbl %>%
+  select(home_winner_response
+         , contains("roll5")
+         , -id
+         , home_distance
+         , away_distance
+         # , home_ap_rank_fct
+         # , away_ap_rank_fct
+  )
+names(standard_rolling_test_tbl) <- gsub("roll5", "", names(standard_rolling_test_tbl))
+
+standard_rolling_validation_tbl <- validation_tbl %>%
+  select(home_winner_response
+         , contains("roll5")
+         , -id
+         , home_distance
+         , away_distance
+         # , home_ap_rank_fct
+         # , away_ap_rank_fct
+  )
+names(standard_rolling_validation_tbl) <- gsub("roll5", "", names(standard_rolling_validation_tbl))
+
 # Read in Model Data ----
 ## Create Season, Rolling and Standard Training
 baseline_rolling_logit_model <- readr::read_rds("Data/models/baseline_rolling_logit_model.rds")
@@ -88,10 +132,86 @@ model_tbl <- lapply(ls(), create_tibble) %>%
   )  %>%
   ungroup() %>%
   select(-model)  %>%
-  separate(name, into = c("Simplicity","Metric","Model Type","Temp"), remove = FALSE)
+  separate(name, into = c("Simplicity","Metric","Model Type","Temp"), remove = FALSE) %>%
+  filter(!Metric %in% "standard")
+# model_standard_season_tbl <- lapply(ls(), create_tibble) %>% 
+#   bind_rows() %>% 
+#   rowwise() %>% 
+#   mutate(model = list(get(name))) %>%
+#   filter(stringr::str_detect(name,"_model")) %>%
+#   separate(name, into = c("Simplicity","Metric","Model Type","Temp"), remove = FALSE) %>%
+#   filter(Metric %in% "standard") %>%
+#   rowwise() %>% 
+#   # select(-model) %>%
+#   # mutate(check = stringr::str_detect(Simplicity,"enhanced"))
+#   mutate(
+#     pred_test = ifelse(stringr::str_detect(Simplicity,"enhanced")
+#                        , list(predict(model, standard_season_test_tbl, type = 'prob') %>% pull(2) %>% round())
+#                        , list(predict(model, standard_season_test_tbl, type = 'response') %>% as.vector() %>% round() )
+#                        )
+#     
+#     , pred_validation = ifelse(stringr::str_detect(Simplicity,"enhanced"), list(predict(model, standard_season_validation_tbl, type = 'prob') %>% pull(2) %>% round() ), list(predict(model, standard_season_validation_tbl, type = 'response') %>% as.vector() %>% round() ))
+#     , response_test = list(standard_season_test_tbl %>% pull(home_winner_response) %>% as.numeric() %>% -1)
+#     , response_validation = list(standard_season_validation_tbl %>% pull(home_winner_response) %>% as.numeric() %>% -1)
+#     , test_confusion_matrix = list(caret::confusionMatrix(reference = response_test %>% factor(), data=pred_test %>% factor()))
+#     , validation_confusion_matrix = list(caret::confusionMatrix(reference = response_validation %>% factor(), data=pred_validation %>% factor()))
+#   )  %>%
+#   ungroup() %>%
+#   select(-model) %>%
+#   mutate(Metric = "standard-season")
+# 
+# model_standard_rolling_tbl <- lapply(ls(), create_tibble) %>% 
+#   bind_rows() %>% 
+#   rowwise() %>% 
+#   mutate(model = list(get(name))) %>%
+#   filter(stringr::str_detect(name,"_model")) %>%
+#   separate(name, into = c("Simplicity","Metric","Model Type","Temp"), remove = FALSE) %>%
+#   filter(Metric %in% "standard") %>%
+#   rowwise() %>% 
+#   # select(-model) %>%
+#   # mutate(check = stringr::str_detect(Simplicity,"enhanced"))
+#   mutate(
+#     pred_test = ifelse(stringr::str_detect(Simplicity,"enhanced")
+#                        , list(predict(model, standard_rolling_test_tbl, type = 'prob') %>% pull(2) %>% round())
+#                        , list(predict(model, standard_rolling_test_tbl, type = 'response') %>% as.vector() %>% round() )
+#     )
+#     
+#     , pred_validation = ifelse(stringr::str_detect(Simplicity,"enhanced"), list(predict(model, standard_rolling_validation_tbl, type = 'prob') %>% pull(2) %>% round() ), list(predict(model, standard_rolling_validation_tbl, type = 'response') %>% as.vector() %>% round() ))
+#     , response_test = list(standard_rolling_test_tbl %>% pull(home_winner_response) %>% as.numeric() %>% -1)
+#     , response_validation = list(standard_rolling_validation_tbl %>% pull(home_winner_response) %>% as.numeric() %>% -1)
+#     , test_confusion_matrix = list(caret::confusionMatrix(reference = response_test %>% factor(), data=pred_test %>% factor()))
+#     , validation_confusion_matrix = list(caret::confusionMatrix(reference = response_validation %>% factor(), data=pred_validation %>% factor()))
+#   )  %>%
+#   ungroup() %>%
+#   select(-model) %>%
+#   mutate(Metric = "standard-rolling")
+
+# model_tbl_final <- bind_rows(model_tbl,model_standard_season_tbl,model_standard_rolling_tbl)
 
 # readr::write_rds(model_tbl, "Data/clean/model_prediction_tbl.rds")
 
+# Final Predictions Table ----
+model_tbl_final <- model_tbl %>%
+  ungroup() %>%
+  select(-test_confusion_matrix
+         ,-validation_confusion_matrix
+         ) %>%
+  pivot_longer(c(-name,-Simplicity,-Metric,-`Model Type`,-Temp)
+               , names_to = "temp",values_to = "value") %>%
+  separate(temp,into = c("pred","type")) %>%
+  pivot_wider(names_from = pred
+              , values_from = value) %>% 
+  rowwise() %>%
+  mutate(confusion_matrix_ = list(caret::confusionMatrix(reference = response %>% unlist() %>% factor()
+                                                   , data=pred %>% unlist()%>% factor() ) )
+         , specificity_ = caret::specificity(reference = response %>% unlist() %>% factor()
+                                             , data=pred %>% unlist()%>% factor())
+         , sensitivity_ = caret::sensitivity(reference = response %>% unlist() %>% factor()
+                                             , data=pred %>% unlist()%>% factor())
+         , accuracy_ = confusion_matrix_$overall[[1]] %>% round(3) %>% .[1]
+         ) %>%
+  select(-Temp,-name)
+  
 # Create Ensemble Prediction Tibble ----
 create_tibble <- function(x){
   
@@ -119,9 +239,59 @@ model_ensemble_tbl <- lapply(ls(), create_tibble) %>%
   summarize(pred_test = list(reduce(pred_test, element_wise_add)) %>% lapply(function(x) round(x/10))
             , pred_validation = list(reduce(pred_validation, element_wise_add)) %>% lapply(function(x) round(x/10))
             , response_test = list(reduce(response_test, element_wise_add)) %>% lapply(function(x) round(x/10))
-            , response_validation = list(reduce(response_validation, element_wise_add)) %>% lapply(function(x) round(x/10))
-
-  ) %>%
+            , response_validation = list(reduce(response_validation, element_wise_add)) %>% lapply(function(x) round(x/10))) %>%
   mutate(test_confusion_matrix = list(caret::confusionMatrix(reference = response_test  %>% unlist() %>% factor(), data=pred_test  %>% unlist() %>% factor()))
          , validation_confusion_matrix = list(caret::confusionMatrix(reference = response_validation  %>% unlist() %>% factor(), data=pred_validation  %>% unlist() %>% factor())))
-# readr::write_rds(model_tbl, "Data/clean/model_prediction_tbl.rds")
+
+model_ensemble_tbl <- model_ensemble_tbl %>%
+  select(-test_confusion_matrix, -validation_confusion_matrix) %>%
+  pivot_longer(everything()) %>%
+  separate(name, into=c("temp","type")) %>%
+  pivot_wider(names_from = temp, values_from = value) %>%
+  mutate(Simplicity = "combination"
+         , Metric = "combination"
+         , `Model Type` = "combination"
+         ) %>%
+  rowwise() %>%
+  mutate(confusion_matrix_ = list(caret::confusionMatrix(reference = response %>% unlist() %>% factor()
+                                                         , data=pred %>% unlist()%>% factor() ) )
+         , specificity_ = caret::specificity(reference = response %>% unlist() %>% factor()
+                                             , data=pred %>% unlist()%>% factor())
+         , sensitivity_ = caret::sensitivity(reference = response %>% unlist() %>% factor()
+                                             , data=pred %>% unlist()%>% factor())
+         , accuracy_ = confusion_matrix_$overall[[1]] %>% round(3) %>% .[1]
+  )
+
+espn_prob_tmp1 <- test_tbl %>% 
+  select(home_espn_probability, home_winner_response) %>%
+  mutate(home_espn_probability = round(home_espn_probability/100)
+         , home_winner_response = home_winner_response %>% as.numeric() -1 
+  )
+espn_prob_tmp2 <- validation_tbl %>% 
+  select(home_espn_probability, home_winner_response) %>%
+  mutate(home_espn_probability = round(home_espn_probability/100)
+         , home_winner_response = home_winner_response %>% as.numeric() -1 
+  )
+
+
+model_tbl_final <- bind_rows(model_ensemble_tbl
+          , model_tbl_final
+          , tibble(type = c("test","validation")
+                   , pred = c(list(espn_prob_tmp1$home_espn_probability),list(espn_prob_tmp2$home_espn_probability)
+                   )
+                   , response = c(list(espn_prob_tmp1$home_winner_response),list(espn_prob_tmp2$home_winner_response)
+                   )
+                   , Simplicity = "espn"
+                   , Metric = "espn"
+                   , `Model Type` = "espn"
+          )%>%
+            rowwise() %>%
+            mutate(confusion_matrix_ = list(caret::confusionMatrix(reference = response %>% unlist() %>% factor()
+                                                                   , data=pred %>% unlist()%>% factor() ) )
+                   , specificity_ = caret::specificity(reference = response %>% unlist() %>% factor()
+                                                       , data=pred %>% unlist()%>% factor())
+                   , sensitivity_ = caret::sensitivity(reference = response %>% unlist() %>% factor()
+                                                       , data=pred %>% unlist()%>% factor())
+                   , accuracy_ = confusion_matrix_$overall[[1]] %>% round(3) %>% .[1]
+            )
+          ) 
